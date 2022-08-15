cbde75971c2f62375bc2ddfe18719cb3a5f51e07
EbriusQuasimodo <sdohnisukaloveu@gmail.com>
2022-08-15 16:38:00 +0500

    Add home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -8,45 +9,46 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  //int currentPage = 1;
+  int currentPage = 1;
+
 
   @override
   Widget build(BuildContext context) {
     return
         Scaffold(
             body: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 9,
-            ),
-                  child: GridView.builder(
-                      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                        maxCrossAxisExtent: 310,
-                        childAspectRatio: 1.9 / 3,
-                        crossAxisSpacing: 25,
-                        mainAxisSpacing: 3,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 9,
+              ),
+                    child: GridView.builder(
+                        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                          maxCrossAxisExtent: 310,
+                          childAspectRatio: 1.9 / 3,
+                          crossAxisSpacing: 25,
+                          mainAxisSpacing: 3,
+                        ),
+                        itemCount: 10,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            alignment: Alignment.center,
+                            child: InkWell(
+                              onTap: () {},
+                              child: Column(
+                                children: [ClipRRect(
+                                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                                    child: Text('model.meme.'),),
+                                  SizedBox(height: 10),
+
+                        ],
                       ),
-                      itemCount: 10,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Container(
-                          alignment: Alignment.center,
-                          child: InkWell(
-                            onTap: () {},
-                            child: Column(
-                              children: [ClipRRect(
-                                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                                  child: Text('sdasdda'),),
-                                SizedBox(height: 10),
-
-                      ],
                     ),
-                  ),
 
-                );
+                  );
     }
           ),
 
-          ),
+            ),
         );
   }
 }
